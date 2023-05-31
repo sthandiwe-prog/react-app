91810b82faab55ff9d2884584003c72328c6499c
sthandiwe-prog <132610235+sthandiwe-prog@users.noreply.github.com>
2023-05-31 15:43:48 +0200

    Added weather app data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
+  let weatherApp = {
+    humidity: 70,
+    wind: 30,
+    condition: "Mostly sunny",
+    temperature: 23,
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="document-wrapper">
+          <div className="content">
+            <form>
+              <input
+                type="search"
+                placeholder="Search City..."
+                className="search-bar"
+                autocomplete="off"
+              />
+              <input type="submit" value="Search" className="button" />
+            </form>
+
+            <div className="col-6">
+              <p className="city">
+                <span>Durban</span>:
+                <span>
+                  <strong> {weatherApp.temperature} </strong>
+                  <small>
+                    <a href="/" className="celsius">
+                      °C
+                    </a>
+                    |
+                    <a href="/" className="fahrenheit">
+                      °F
+                    </a>
+                  </small>
+                </span>
+                <br />
+                <img
+                  src={weatherApp.imgUrl}
+                  alt="weather icon"
+                  className="weather-icon"
+                />
+              </p>
+            </div>
+
+            <div className="col-6">
+              Weather <br />
+              <br />
+              <span>{weatherApp.condition} </span>
+              <br />
+            </div>
+
+            <div className="conditions">
+              <div>Humidity:{weatherApp.humidity} %</div>
+              <div>Wind:{weatherApp.wind} km/h</div>
+            </div>
+
+            <div className="weather-forecast"></div>
+          </div>
+
+          <small className="code-source">
+            <a
+              href="https://github.com/sthandiwe-prog/my-weather-project"
+              target="_blank"
+              title="my code"
+              rel="noreferrer"
+              className="link"
+            >
+              Open-source code
+            </a>
+            , by Sthandiwe Gasa
+          </small>
+        </div>
+      </div>
+
+      <script src="css-code/index.js"></script>
     </div>
   );
 }
-
-export default App;
